1344cb0d20b5efeb950c01d9908065c75b2bfbc7
Phantoz <tellersecret894@gmail.com>
2025-09-27 03:35:22 +0000

    Top bar should be aligned perfectly Login and logos and etc should be pe

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -13,17 +13,19 @@ export function Header({}: HeaderProps) {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center justify-between">
-        <div className="flex items-center gap-2 cursor-pointer" onClick={() => router.push('/')}>
+      <div className="container flex h-16 items-center">
+        <div className="mr-auto flex items-center gap-2 cursor-pointer" onClick={() => router.push('/')}>
           <Eye className="h-7 w-7 text-primary" />
           <h1 className="text-2xl font-bold font-logo">
             <span className="text-primary">Focus Case</span><span className="text-foreground relative">X</span>
           </h1>
         </div>
-        <Button size="sm" variant="outline" onClick={() => router.push('#')}>
-          <LogIn className="mr-2 h-4 w-4" />
-          Login
-        </Button>
+        <nav className="flex items-center gap-4">
+            <Button size="sm" variant="outline" onClick={() => router.push('#')}>
+              <LogIn className="mr-2 h-4 w-4" />
+              Login
+            </Button>
+        </nav>
       </div>
     </header>
   );
