19bbb240634dcbc9000b85ae330c96e0f3a371ad
Phantoz <tellersecret894@gmail.com>
2025-11-18 16:24:40 +0000

    Not in env within code itself ise api key

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,7 +1,4 @@
 
-import { config } from 'dotenv';
-config();
-
 import '@/ai/flows/analyze-optometry-case.ts';
 import '@/ai/flows/extract-case-insights.ts';
 import '@/ai/flows/chat-with-case-flow.ts';
--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiKey: 'AIzaSyBwlyo5Mml52n8RriwaFRnimEJF1XgF0j4'})],
   model: 'googleai/gemini-2.5-pro',
 });
